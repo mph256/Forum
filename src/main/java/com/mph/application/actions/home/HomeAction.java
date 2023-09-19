4343f6d29c5b023c2e62bc03cb207b4670a16ee8
m <phamichael123@gmail.com>
2023-09-19 05:40:22 +0200

    chore: add folders

---
+++ src/main/java/com/mph/application/actions/home/HomeAction.java
@@ -0,0 +1,9 @@
+package com.mph.application.actions.home;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+}
